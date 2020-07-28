bb2f6d75420109e67882875c5b4b0cbbd5bcbd2d
Michael Rehn <rehn.michael@outlook.com>
2020-07-28 15:36:46 +0200

    Update PS profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,24 @@
-Invoke-Expression (&starship init powershell)
-Set-PSReadLineOption -EditMode "Vi"
 
+Invoke-Expression (&starship init powershell)
+
+# Set vi mode
+Set-PSReadLineOption -EditMode Vi
+$env:VISUAL='nvim'
+# Changes cursor depending on PSReadLines Vi Mode
+function OnViModeChange {
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+Write-Host -NoNewLine "`e[5 q"
+
+# Must be set after Vi mode
+Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Chord Tab -Function MenuComplete
+Set-PSReadLineOption -PredictionSource History 
